fccd20c398e76faa2813801dadfda2a6b259c881
Sylvester Norrbjörk <sn222jn@student.lnu.se>
2013-11-27 11:28:49 +0100

    guess in 1-vandalen is complete

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -2,6 +2,7 @@
 
 window.onload = function () {
     var secret;
+    var count = 0;
 
     secret = Math.floor((Math.random() * 100) + 1);
     //Math.floor(Math.random() * (max - min) + 1) + min;
@@ -12,54 +13,40 @@ window.onload = function () {
         console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
         console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
 
-        var count = 0;
-        var answer = [];
         var replyArr = [];
 
-        replyArr[0] = [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."];
+        number = +number; // convert number(string) to number(int).         
+        count += 1;; // Keeps track of number of guesses.        
+
+        replyArr[0] = [true, "Grattis du vann! Det hemliga talet var " + secret + " och du behövde " + count + " gissningar för att hitta det."];
         replyArr[1] = [false, "Det hemliga talet är högre!"];
         replyArr[2] = [false, "Det hemliga talet är lägre!"];
-        replyArr[3] = [false, "Talet är utanför intervallet 0 - 100"];
-
-        //arr[0] = [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."];
-        //arr[1] = [false, "Det hemliga talet är högre!", "Det hemliga talet är lägre!", "Talet är utanför intervallet 0 - 100"];
+        replyArr[3] = [false, "Talet är utanför intervallet 1 - 100"];
+        replyArr[4] = [false, "Du måste ange ett tal!"];
 
 
-        //do {
-            count += 1;
-            if (isNaN(number)) {
-                return "Du måste ange ett tal!";
+
+        if (isNaN(number)) {
+            count -= 1; // A guess that isn't a number doesn't count as a guess.
+
+            return replyArr[4];
+        }
+
+        else if (number < 1 || number > 100) {
+            count -= 1; // A guess outside the accepted range doesn't count as a guess.
+            return replyArr[3];
+        }
+        else {
+            if (number === secret) {
+                return replyArr[0];
             }
-
-            else if (number < 1 || number > 100) {
-                answer[1] = replyArr[3];
-                return answer[1];
+            else if (number < secret) {
+                return replyArr[1];
             }
-            else {
-                if (number === secret) {
-                    answer[0] = replyArr[0];
-                    return answer[0];
-                }
-                else if (number < secret) {
-                    answer[1] = replyArr[1];
-                    return answer[1];
-                }
-                else if (number > secret) {
-                    answer[1] = replyArr[2];
-                    return answer[1];
-                }
-              
+            else if (number > secret) {
+                return replyArr[2];
             }
-
-            number.reset();
-
-        //} while (number != secret);
-
-        guess();
-
-        
-
-
+        }
 
     };
 
@@ -76,8 +63,11 @@ window.onload = function () {
     submit.addEventListener("click", function (e) {
         e.preventDefault(); // Hindra formuläret från att skickas till servern. Vi hanterar allt på klienten.
 
-        var answer = guess(input.value) // Läser in talet från textrutan och skickar till funktionen "guess"
-        p.innerHTML = answer[1];		// Skriver ut texten från arrayen som skapats i funktionen.	
+        var answer = guess(input.value); // Läser in talet från textrutan och skickar till funktionen "guess"
+
+        p.innerHTML = answer[1];		// Skriver ut texten från arrayen som skapats i funktionen.
+
+        input.value = ""; // Clears the text input box after each guess.
 
         if (answer[0] === true) {				// Om spelet är slut, avaktivera knappen.
             submit.disabled = true;
